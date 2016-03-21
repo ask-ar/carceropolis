84551eaf0c9b00d3c5a34d5a34eb6069d26b7304
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-03-21 11:46:18 -0300

    add loginRequired control

--- lib/routes.jsx
+++ lib/routes.jsx
@@ -7,3 +7,9 @@ Router.map(function(){
   this.route('home', { path: "/" });
 
 });
+
+var loginRequired = function(pause) {
+  (Meteor.user() || Meteor.loggingIn()) ? this.next() : Router.go('login');
+};
+
+// Router.onBeforeAction(loginRequired, {only: []});
