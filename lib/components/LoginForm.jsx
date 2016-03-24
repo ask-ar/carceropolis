041cb3f777fe826994d3cbfee35b3ca13ba57e00
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-03-24 17:30:30 -0300

    improve styling + login form

---
+++ lib/components/LoginForm.jsx
@@ -0,0 +1,36 @@
+LoginForm = React.createClass({
+  handleSubmit(event) {
+    event.preventDefault();
+
+    var email = event.target.email.value;
+    var password = event.target.password.value;
+
+    Meteor.loginWithPassword(email, password, err => {
+      if (err) {
+        event.target.password.value = "";
+        FlashMessages.sendSuccess("Usuário ou senha incorretos!");
+
+      } else {
+        event.target.reset();
+        Router.go('home');
+      }
+    });
+  },
+
+  render() {
+    return (
+      <div id="login" className='row'>
+        <form className="login col-md-6 col-md-offset-3" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input type="email" name="email" className="form-control email" placeholder="E-mail Adress" required />
+          </div>
+          <div className="form-group">
+            <input type="password" name="password" className="form-control password" placeholder="Password" required />
+          </div>
+
+          <button className="btn submit-btn" type="submit" value="Register">ACESSAR</button>
+        </form>
+      </div>
+    );
+  }
+});
