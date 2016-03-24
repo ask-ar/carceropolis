7f330a8807af395dc15ba91334a18823f87fd1b0
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-03-24 17:00:39 -0300

    responsive menu

--- lib/components/Navbar.jsx
+++ lib/components/Navbar.jsx
@@ -29,10 +29,10 @@ Navbar = React.createClass({
     $("#nav-menu").offcanvas('hide');
   },
 
-  render() {
+  menuSmall() {
     const navbarCls = `navbar navbar-default navbar-fixed-top ${this.isHome() ? 'home' : ''}`;
     return (
-      <div>
+      <div className='hidden-md hidden-lg cp-menu-small'>
         <nav id="nav-menu" className="navmenu navmenu-default navmenu-fixed-left offcanvas" role="navigation">
           <h2 className="navmenu-brand">MENU</h2>
           <ul className="nav navmenu-nav">
@@ -55,6 +55,39 @@ Navbar = React.createClass({
         <div className={navbarCls} >
           <a href="#" className="menu-icon" data-toggle="offcanvas" data-target="#nav-menu" data-canvas="body"><i className="glyphicon glyphicon-menu-hamburger"></i></a>
         </div>
+      </div>
+    );
+  },
+
+  menuLarge() {
+    return (
+      <div className='hidden-xs hidden-sm cp-menu-large'>
+        <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+          <a href="/" className="navbar-brand">Carceropólis</a>
+          <ul className="nav navbar-nav">
+            <li><a href="#">DADOS</a></li>
+            <li><a href="#">PUBLICAÇÕES</a></li>
+
+            <li><a href="#">BANCO DE ESPECIALISTAS</a></li>
+            <li><a href="#">FALE CONOSCO</a></li>
+            <li><a href="#">SOBRE NÓS</a></li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right navbar-actions">
+            { this.loggedIn() ? <li><a href="/adm">Admin</a></li> : false }
+            { this.loggedIn() ? <li><a href="#" onClick={this.handleLogout}>Logout</a></li> : false }
+
+            { !this.loggedIn() ? <li><a href="/login" onClick={this.handleLogin}>Login</a></li> : false }
+          </ul>
+        </nav>
+      </div>
+    );
+  },
+
+  render() {
+    return (
+      <div>
+        {this.menuSmall()}
+        {this.menuLarge()}
       </div>
     );
   },
