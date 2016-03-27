2c7a75815e64b931f817742e10e7f21b04e16af1
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-03-26 23:26:17 -0300

    home page (with video, overlay and responsiveness)

--- lib/components/Home.jsx
+++ lib/components/Home.jsx
@@ -8,10 +8,23 @@ Home = React.createClass({
   },
 
   render() {
-    const msg = 'Welcome HOME' + (this.data.user ? `, ${this.data.user.username}` : '');
     return (
       <div className="home">
-        {msg} !
+        <video autoPlay loop muted poster="/presidio.jpg" className="home--bgvid">
+          <source src="/ff.webm" type="video/webm" />
+        </video>
+        <div className="home--overlay"></div>
+        <div className="home--content col-md-6 col-md-offset-3">
+          <div className="home--brand">Carcerópolis</div>
+          <p className="home--teaser">
+            Single-origin coffee put a bird on it craft beer YOLO, Portland hella deep v Schlitz. Tumblr Bushwick post-ironic Thundercats. Vinyl 90's keytar, literally cardigan Williamsburg YOLO squid pickled Etsy salvia lo-fi locavore. Meh leggings retro
+narwhal Neutra
+          </p>
+          <div className="home--links">
+            <a className="btn btn-large" href="/dados">DADOS</a>
+            <a className="btn btn-large" href="/posts">PUBLICAÇÕES</a>
+          </div>
+        </div>
       </div>
     );
   }
