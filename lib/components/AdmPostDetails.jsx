e1ff9ce48affd86d07b5ec9adfe06b5951349b5e
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-04-02 13:03:08 -0300

    change post header if the post has an image

--- lib/components/AdmPostDetails.jsx
+++ lib/components/AdmPostDetails.jsx
@@ -9,23 +9,29 @@ AdmPostDetails = React.createClass({
     };
   },
 
-  componentDidMount() {
-    // const titleImg = $(this.data.post.content).find('img').first();
-    // $('.post--title img').attr('src', titleImg.attr('src'));
-  },
-
   render() {
     if (!this.data.post) { return false; }
 
     const post = this.data.post;
     const markup = {__html: post.content};
 
+    // get the first image to be the post header background
+    const titleImg = $(this.data.post.content).find('img').first();
+    var headerStyle = {};
+    if (titleImg.length > 0 ) {
+      headerStyle['backgroundImage'] = "url('" + titleImg.attr("src") + "')";
+    }
+
     return (
       <div className="adm">
-        <div><a href="/adm">&laquo; retornar</a></div>
+        <div className="post-details-actions col-md-6 col-md-offset-3">
+          <a href="/adm" className="btn btn-default">&laquo; retornar</a>
+          <button className="btn btn-action btn-red"><i className="glyphicon glyphicon-trash"></i></button>
+          <button className="btn btn-action btn-primary">Editar</button>
+        </div>
 
         <div className="post col-md-6 col-md-offset-3">
-          <div className="post--header"></div>
+          <div className="post--header" style={headerStyle}></div>
           <div className="post--title"><h2>{post.title}</h2></div>
           <aside>
             por <span className="post--author">{post.username}</span>
