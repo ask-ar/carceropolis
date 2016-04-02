fdae1d2d481b02efdad8b8bef9fc9dbdaf6f77fc
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2016-04-02 12:34:52 -0300

    post details syling

--- lib/components/AdmPostDetails.jsx
+++ lib/components/AdmPostDetails.jsx
@@ -9,6 +9,11 @@ AdmPostDetails = React.createClass({
     };
   },
 
+  componentDidMount() {
+    // const titleImg = $(this.data.post.content).find('img').first();
+    // $('.post--title img').attr('src', titleImg.attr('src'));
+  },
+
   render() {
     if (!this.data.post) { return false; }
 
@@ -16,14 +21,15 @@ AdmPostDetails = React.createClass({
     const markup = {__html: post.content};
 
     return (
-      <div className="adm-post post-details col-md-8 col-md-offset-2">
+      <div className="adm">
         <div><a href="/adm">&laquo; retornar</a></div>
 
-        <div className="post">
+        <div className="post col-md-6 col-md-offset-3">
+          <div className="post--header"></div>
           <div className="post--title"><h2>{post.title}</h2></div>
           <aside>
             por <span className="post--author">{post.username}</span>
-            <span className="post--time">{moment(post.createdAt).format('DD/MM/YYYY HH:MM')}</span>
+          em <span className="post--time">{moment(post.createdAt).format('DD/MM/YYYY HH:MM')}</span>
           </aside>
           <div className="post--content" dangerouslySetInnerHTML={markup} />
         </div>
