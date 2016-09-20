20d733b2f000ad0c77cf7c2171b646e278df5972
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2016-09-20 13:32:07 -0300

    Adicionando models e admin para:
     - Área de atuação (Categoria)
     - Especialidade
     - Especialista
     - Publicacao
    
    Signed-off-by: Diego Rabatone Oliveira <diraol@diraol.eng.br>

--- carceropolis_theme/models.py
+++ carceropolis_theme/models.py
@@ -1,1 +1,53 @@
+# coding= utf-8
+"""Modelos definidos para o Projeto carcerópolis."""
+from django.db import models
+from mezzanine.blog.models import BlogPost
+from phonenumber_field.modelfields import PhoneNumberField
 
+
+CATEGORIAS = (
+    ('GERAL', 'GERAL'),
+    ('SISTEMA', 'FUNCIONAMENTO DO SISTEMA'),
+    ('PERFIL', 'PERFIL POPULACIONAL'),
+    ('POLITICA', 'POLITICA CRIMINAL'),
+    ('INTERNACIONAL', 'SISTEMAS INTERNACIONAIS'),
+    ('VIOLENCIA', 'VIOLENCIA INSTITUCIONAL')
+)
+
+
+class AreaDeAtuacao(models.Model):
+    """Categorias Gerais de classificação de Especialistas e Publicações."""
+    nome_da_area = models.CharField(max_length=250)
+    descricao = models.TextField()
+
+    def __unicode__(self):
+        return self.nome_da_area
+
+
+class Especialidade(models.Model):
+    """Definição das Especialidades principais mapeadas no projeto."""
+    nome_da_especialidade = models.CharField(max_length=35)
+    descricao = models.TextField(blank=True)
+    # slug = models.CharField(max_length=250)
+
+    def __unicode__(self):
+        return self.nome_da_especialidade
+
+
+class Especialista(models.Model):
+    """Classe que define os especialistas para o 'Banco de Especialistas'."""
+    nome = models.CharField(max_length=250)
+    email = models.EmailField()
+    telefone = PhoneNumberField(blank=True)
+    mini_bio = models.CharField(max_length=250, blank=True)
+    instituicao = models.CharField(max_length=250)
+    area_de_atuacao = models.ManyToManyField(AreaDeAtuacao)
+    especialidades = models.ManyToManyField(Especialidade)
+
+    def __unicode__(self):
+        return self.nome
+
+
+class Publicacao(BlogPost):
+    """Publicações relacionadas à temática do site."""
+    categorias = models.ManyToManyField(AreaDeAtuacao)
