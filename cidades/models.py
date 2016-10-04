0ff86ec62a779165275b005d82527dc476b2b373
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2016-10-04 02:09:58 -0300

    Adding "cidades" app
    Signed-off-by: Diego Rabatone Oliveira <diraol@diraol.eng.br>

---
+++ cidades/models.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+#
+# Thanks to: https://gist.github.com/JhonatasMartins/7eed599a2f95d005b81f
+#
+from django.db import models
+
+STATE_CHOICES = (
+    ('AC', 'Acre'), ('AL', 'Alagoas'), ('AP', 'Amapá'),
+    ('AM', 'Amazonas'), ('BA', 'Bahia'), ('CE', 'Ceará'),
+    ('DF', 'Distrito Federal'), ('ES', 'Espírito Santo'),
+    ('GO', 'Goiás'), ('MA', 'Maranhão'), ('MT', 'Mato Grosso'),
+    ('MS', 'Mato Grosso do Sul'), ('MG', 'Minas Gerais'),
+    ('PA', 'Pará'), ('PB', 'Paraíba'), ('PR', 'Paraná'),
+    ('PE', 'Pernambuco'), ('PI', 'Piauí'), ('RJ', 'Rio de Janeiro'),
+    ('RN', 'Rio Grande do Norte'), ('RS', 'Rio Grande do Sul'),
+    ('RO', 'Rondônia'), ('RR', 'Roraima'), ('SC', 'Santa Catarina'),
+    ('SP', 'São Paulo'), ('SE', 'Sergipe'), ('TO', 'Tocantins')
+)
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=60)
+    estado = models.CharField(max_length=2, choices=STATE_CHOICES)
+
+    def __unicode__(self):
+        return self.nome
