5e5dc1e0ad85967fa6b50449e0f01a713242753e
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2017-02-27 12:28:22 -0300

    Adding logHistory menu item on admin page

--- carceropolis/admin.py
+++ carceropolis/admin.py
@@ -8,6 +8,11 @@ from mezzanine.blog.admin import BlogPostAdmin
 from .models import (AreaDeAtuacao, BaseMJ, Especialidade, Especialista,
                      Publicacao, UnidadePrisional)
 
+from django.contrib.admin.models import LogEntry, DELETION
+from django.utils.html import escape
+from django.core.urlresolvers import reverse
+
+
 
 class EspecialistaAdmin(admin.ModelAdmin):
     search_fields = ['nome', 'instituicao', 'area_de_atuacao__nome',
@@ -81,6 +86,62 @@ class BaseMJAdmin(admin.ModelAdmin):
         else:
             return []
 
+class LogEntryAdmin(admin.ModelAdmin):
+
+    date_hierarchy = 'action_time'
+
+    readonly_fields = [f.name for f in LogEntry._meta.get_fields()]
+
+    list_filter = [
+        'user',
+        'content_type',
+        'action_flag'
+    ]
+
+    search_fields = [
+        'object_repr',
+        'change_message'
+    ]
+
+
+    list_display = [
+        'action_time',
+        'user',
+        'content_type',
+        'object_link',
+        'action_flag',
+        'change_message',
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser and request.method != 'POST'
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def object_link(self, obj):
+        if obj.action_flag == DELETION:
+            link = escape(obj.object_repr)
+        else:
+            ct = obj.content_type
+            link = u'<a href="%s">%s</a>' % (
+                reverse('admin:%s_%s_change' % (ct.app_label, ct.model), args=[obj.object_id]),
+                escape(obj.object_repr),
+            )
+        return link
+    object_link.allow_tags = True
+    object_link.admin_order_field = 'object_repr'
+    object_link.short_description = u'object'
+
+    def queryset(self, request):
+        return super(LogEntryAdmin, self).queryset(request) \
+            .prefetch_related('content_type')
+
+
+admin.site.register(LogEntry, LogEntryAdmin)
 
 admin.site.register(AreaDeAtuacao)
 admin.site.register(BaseMJ, BaseMJAdmin)
