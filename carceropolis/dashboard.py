fbf251d47b2deb4600e286c5fab12cc3cc82ef92
AndresMRM <andres@inventati.org>
2017-08-07 11:48:23 -0300

    bokeh autoreload server

--- carceropolis/dashboard.py
+++ carceropolis/dashboard.py
@@ -17,14 +17,14 @@ def modify_doc(doc):
     data_url = "http://www.neracoos.org/erddap/tabledap/B01_sbe37_all.csvp?time,temperature&depth=1&temperature_qc=0&time>=2016-02-15&time<=2017-03-22"
     b, i, q = data_url.partition('?')
     data_url = b+i+requests.utils.quote(q)
-    # data_url = b+i+urllib.parse.quote(q)
     df = pd.read_csv(data_url, parse_dates=True, index_col=0)
     df = df.rename(columns={'temperature (celsius)': 'temperature'})
     df.index.name = 'time'
 
     source = ColumnDataSource(data=df)
 
-    plot = figure(x_axis_type='datetime', y_range=(0, 25), y_axis_label='Temperature (Celsius)',
+    plot = figure(x_axis_type='datetime', y_range=(0, 25),
+                  y_axis_label='Temperature (Celsius)',
                   title="Sea Surface Temperature at 43.18, -70.43")
     plot.line('time', 'temperature', source=source)
 
@@ -35,7 +35,8 @@ def modify_doc(doc):
             data = df.rolling('{0}D'.format(new)).mean()
         source.data = ColumnDataSource(data=data).data
 
-    slider = Slider(start=0, end=30, value=0, step=1, title="Smoothing by N Days")
+    slider = Slider(start=0, end=30, value=0, step=1,
+                    title="Smoothing by N days")
     slider.on_change('value', callback)
 
     doc.add_root(column(slider, plot))
@@ -43,7 +44,7 @@ def modify_doc(doc):
     doc.theme = Theme(json=yaml.load("""
         attrs:
             Figure:
-                background_fill_color: "#DDDDDD"
+                background_fill_color: "#000000"
                 outline_line_color: white
                 toolbar_location: above
                 height: 500
@@ -57,11 +58,24 @@ def modify_doc(doc):
 class BackgroundBokeh(threading.Thread):
     def run(self):
         bokeh_app = Application(FunctionHandler(modify_doc))
-        io_loop = IOLoop()
-        server = Server({'/bkapp': bokeh_app}, io_loop=io_loop, allow_websocket_origin=["localhost:8000"])
+        io_loop = IOLoop.current()
+        server = Server({'/bkapp': bokeh_app}, io_loop=io_loop,
+                        allow_websocket_origin=["localhost:8000"])
         server.start()
         io_loop.start()
 
+    def stop(self):
+        IOLoop.current().stop()
+
+
+bokeh_server = BackgroundBokeh()
+
 
 def start_server():
-    BackgroundBokeh().start()
+    print('= Starting Bokeh server')
+    bokeh_server.start()
+
+
+def stop_server():
+    print('= Stopping Bokeh server')
+    bokeh_server.stop()
--- carceropolis/management/commands/runserver.py
+++ carceropolis/management/commands/runserver.py
@@ -1,9 +1,22 @@
+import sys
+
 from mezzanine.core.management.commands.runserver import Command as BaseRunserverCommand
+
 from carceropolis import dashboard
 
 
 class Command(BaseRunserverCommand):
 
     def run(self, **options):
+        try:
+            super(Command, self).run(**options)
+        except (KeyboardInterrupt, SystemExit):
+            dashboard.stop_server()
+            sys.exit(3)
+        # TODO: This is run multiple times before and after the dashboard
+        # started.
+        dashboard.stop_server()
+
+    def inner_run(self, **options):
         dashboard.start_server()
-        super(Command, self).run(**options)
+        super(Command, self).inner_run(**options)
