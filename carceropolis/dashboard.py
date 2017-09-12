9b783026f21e954b1f89161636946230f73bfff3
AndresMRM <andres@inventati.org>
2017-09-12 15:12:23 -0300

    Allow multiple vars on Y axis (close #78)

--- carceropolis/dashboard.py
+++ carceropolis/dashboard.py
@@ -33,12 +33,18 @@ def update_querystring(window=None, cb_obj=None):
 
 
 def load_db():
+    '''
+    Loads test data for development.
+    '''
     df = pd.read_csv('test.csv')
     df.ano = df.ano.astype(str)
     return df
 
 
 def hide_widget(widget):
+    '''
+    Hides a widget.
+    '''
     if not widget.css_classes:
         widget.css_classes = []
     if 'hidden' not in widget.css_classes:
@@ -46,61 +52,75 @@ def hide_widget(widget):
 
 
 def show_widget(widget):
+    '''
+    Shows a widget.
+    '''
     if widget.css_classes and 'hidden' in widget.css_classes:
         widget.css_classes.remove('hidden')
 
 
-def plot_lines(fig, x, ys, source, palette):
-    for y, color in zip(ys, palette):
-        legend = y+' ' if len(ys) > 1 else None
-        fig.line(x, y, source=source, line_width=3, color=color, legend=legend)
-    # fig.line(x, 'pop_fem', source=source, line_width=3)
+def get_legend(y, ys):
+    '''
+    Return a legend string for a column.
+    An empty space is added to avoid Bokeh behavior of replacing
+    the string with the column data when the name matches.
+    If only one column will be plotted, uses empty legend.
+    '''
+    return y+' ' if len(ys) > 1 else None
 
 
-def plot_vbar(fig, x, ys, source, palette):
-    # fig.vbar(x, 0.5, y, source=source, legend="b")
-    # fig.vbar(x, 0.5, y, source=source, legend='m', line_dash='dotted')
-    # fig.vbar(x, 0.5, 'pop_fem', source=source, legend="f", color='red')
-    # source.data['dois'] = [[115, 8], [80, 25], [240, 20]]
-    # print(source.data)
-    # from bokeh.transform import factor_cmap
-    # fig.vbar(x='cats', width=0.5, top='tops', source=source, line_color="white",
-    #         fill_color=factor_cmap('cats', palette=palette, factors=['pop_masc', 'pop_fem'], start=1, end=2))
-
-    # Bar width including margins
-    outer_width = 0.25
-    # Painted bar area
-    inner_width = outer_width - 0.03
+def plot_bar_iterator(ys, outer_width, palette):
+    '''
+    Helper function that generates values used to plot bars.
+    '''
     l = len(ys)
     for y, color, i in zip(ys, palette, range(0, l)):
         # Spreads bars based on the number of bars and their
         # width, so they don't overlap
         offset = round(-outer_width*l/2 + outer_width/2 + outer_width*i, 2)
-        legend = y+' ' if l > 1 else None
+        yield y, offset, color
+
+
+def plot_lines(fig, x, ys, source, palette):
+    '''
+    Plot a line chart.
+    '''
+    for y, color in zip(ys, palette):
+        fig.line(
+            x, y, source=source, line_width=3, color=color,
+            legend=get_legend(y, ys))
+
+
+def plot_vbar(fig, x, ys, source, palette):
+    '''
+    Plot a vertical bar chart.
+    '''
+    width = 0.2
+    for y, offset, color in plot_bar_iterator(ys, width+.03, palette):
         fig.vbar(
-            x=dodge(x, offset, range=fig.x_range), width=inner_width, top=y,
-            source=source, color=color, legend=legend)
+            x=dodge(x, offset, range=fig.x_range), width=width+.03, top=y,
+            source=source, color=color, legend=get_legend(y, ys))
 
 
 def plot_hbar(fig, x, ys, source, palette):
-    # fig.hbar(y=y, height=0.5, right=x, left=0, source=source)
-    # Bar width including margins
-    outer_width = 0.25
-    # Painted bar area
-    inner_width = outer_width - 0.03
-    l = len(ys)
-    for y, color, i in zip(ys, palette, range(0, l)):
-        offset = round(-outer_width*l/2 + outer_width/2 + outer_width*i, 2)
-        legend = y+' ' if l > 1 else None
+    '''
+    Plot a horizontal bar chart.
+    '''
+    width = 0.2
+    for y, offset, color in plot_bar_iterator(ys, width+.03, palette):
         fig.hbar(
-            y=dodge(x, offset, range=fig.y_range), height=inner_width, right=y,
-            source=source, color=color, legend=legend, )
+            y=dodge(x, offset, range=fig.y_range), height=width, right=y,
+            source=source, color=color, legend=get_legend(y, ys))
 
 
 def plot_circles(fig, x, ys, source, palette):
+    '''
+    Plot a scatter chart.
+    '''
     for y, color in zip(ys, palette):
-        legend = y+' ' if len(ys) > 1 else None
-        fig.circle(x, y, size=10, source=source, color=color, legend=legend)
+        fig.circle(
+            x, y, size=10, source=source, color=color,
+            legend=get_legend(y, ys))
 
 
 class Dashboard(object):
@@ -175,7 +195,8 @@ class Dashboard(object):
                 'value': ['pop_masc'],
                 'options': self.columns,
                 'class': MultiSelect,
-                # 'multiple': True,
+                'querystring_extract_fn':
+                    lambda s: s.split(','),
             },
             'chart_type_sel': {
                 'name': 'type',
@@ -207,6 +228,8 @@ class Dashboard(object):
                 'step': 1,
                 'value': [1, 9],
                 'class': RangeSlider,
+                'querystring_extract_fn':
+                    lambda s: tuple(float(i) for i in s.split(',')),
             },
         }
 
@@ -218,11 +241,12 @@ class Dashboard(object):
                 value = state.get(data['name'])
                 if value:
                     value = value[0]
-                    if data['name'] == 'f_range':
-                        data['value'] = tuple(
-                            float(i) for i in value.split(','))
-                    elif data['name'] == 'y':
-                        data['value'] = value.split(',')
+
+                    # If the model as a special function to extract
+                    # the querystring data, use it.
+                    fn = data.get('querystring_extract_fn')
+                    if fn:
+                        data['value'] = fn(value)
                     else:
                         data['value'] = value
 
@@ -233,25 +257,12 @@ class Dashboard(object):
         for k, v in gui_data.items():
             v = v.copy()
             class_ = v.pop('class')
-            # multiple = v.pop('multiple', None)
+            v.pop('querystring_extract_fn', None)
             control = self.create_control(class_, v)
-            self.controls[k] = control # [control] if multiple else control
+            self.controls[k] = control
 
-        # controls = [getattr(self, k) for k, v in gui_data.items()]
-
-        # for control in controls:
-        #     control.on_change('value', self.update)
-        #     control.callback = js_update_querytstring
-
-        # TODO: seems to have no effect
+        # TODO: seems to have no effect?
         self.controls['filter_range_sel'].callback_policy = 'mouseup'
-
-        # Add one widgetbox for control. Controls with multiple... TODO
-        # widgetboxes = [
-        #     widgetbox(c, width=200)
-        #     if isinstance(c, list) else widgetbox([c], width=200)
-        #     for c in self.controls.values()]
-        # self.layout = row(column(widgetboxes), self.create_figure())
 
         self.layout = row(
             widgetbox(list(self.controls.values()), width=200),
@@ -259,6 +270,9 @@ class Dashboard(object):
         doc.add_root(self.layout)
 
     def create_control(self, class_, args):
+        '''
+        Creates an input widget binding needed callbacks.
+        '''
         control = class_(**args)
         # Bind callbacks
         control.on_change('value', self.update)
@@ -336,20 +350,15 @@ class Dashboard(object):
         df = df.groupby(self.controls['x_sel'].value, as_index=False).sum()
         source = ColumnDataSource(data=df)
 
-        # import itertools
-        # tops = sum(zip(df['pop_masc'], df['pop_fem']), ())
-        # cats = list(itertools.product(df['ano'], ['pop_masc', 'pop_fem']))
-        # source = ColumnDataSource(data={'tops':tops, 'cats':cats})
-
-        chart_type_info = self.chart_types[self.controls['chart_type_sel'].value]
+        chart_type_info = self.chart_types[
+            self.controls['chart_type_sel'].value]
         x_value = self.controls['x_sel'].value
         y_values = self.controls['y_sel'].value
 
         x_title = x_value.title()
         y_title = ', '.join(y.title() for y in y_values)
 
-
-        kw = dict()
+        kw = {}
         if x_value in self.discrete:
             kw['x_range'] = sorted(set(df[x_value]))
         # TODO: os dois eixos podem ter valores discretos?
@@ -357,51 +366,30 @@ class Dashboard(object):
         # if y_value in self.discrete:
         #     kw['y_range'] = sorted(set(ys))
 
-        # import IPython;IPython.embed()
-        # kw['y_range'] = (0, max(sum(y_values, [])))
         kw['y_range'] = (0, max([max(df[y].values) for y in y_values]))
 
         if chart_type_info.get('invert_axies'):
-            # x_value, y_value = y_value, x_value
             x_title, y_title = y_title, x_title
             kw['x_range'], kw['y_range'] = kw['y_range'], kw['x_range']
 
-
-        # xs = df[x_value].values
-        # ys = df[y_value].values
-
-        # y_values = ['pop_masc', 'pop_fem', 'vagas']
-        # y_values = ['pop_masc']
-
-
         kw['title'] = "%s vs %s" % (x_title, y_title)
 
-        # from bokeh.models import FactorRange
-        # fig = figure(x_range=FactorRange(*cats), plot_height=600, plot_width=800,
-        #              tools='pan,box_zoom,reset,save')
         fig = figure(plot_height=600, plot_width=800,
                      tools='pan,box_zoom,reset,save', **kw)
         fig.xaxis.axis_label = x_title
         fig.yaxis.axis_label = y_title
         fig.xgrid.grid_line_color = None
-        # fig.x_range.start = 0
-        # fig.x_range.end = 100
-        # fig.x_range.range_padding = 0.1
 
-        # # TODO: fix needed for hbar, not sure why...
-        # if chart_type_info.get('invert_axies'):
-        #     fig.x_range.start = 0
-        #     fig.x_range.end = xs.max()
-
+        # Rotate category labels so they have more space
         if x_value in self.discrete:
             fig.xaxis.major_label_orientation = pd.np.pi / 4
 
+        # Plot
         chart_type_info['fn'](fig, x_value, y_values, source, self.palette)
 
+        # Tooltips
         tooltips = [(y, '@'+y) for y in y_values]
         tooltips.insert(0, (x_value, '@'+x_value))
-
-        # ("index", "$index"),
         hover = HoverTool(tooltips=tooltips)
         fig.add_tools(hover)
 
