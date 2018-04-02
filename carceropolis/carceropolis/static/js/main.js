c55b57edbb8c95a690c334d226114bc2958ec9f9
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-04-01 21:37:05 -0300

    Ajuste nas animações infográfico #96

--- carceropolis/carceropolis/static/js/main.js
+++ carceropolis/carceropolis/static/js/main.js
@@ -74,118 +74,140 @@ jQuery(function ($) {
     })
 
     // Pagina de dados
-    let animDadosGerais = (ativo = `#dados-gerais`) => {
-        /*
-            1. Dados Gerais
-            - número 622 inciando normal e tornando-se negrito
-            - animação (contador) no 111%
-        */
-        $(`${ativo} [data-tobold]`).addClass(`toBold`)
-        contador(document.querySelectorAll(`${ativo} [data-final-number]`), 12, 1000, ()=>{$(`${ativo} .anim1`).addClass(`bounceInDown`)})
-    }
-
-    $(document).ready(() => animDadosGerais())
-
-    $('body.dados').scrollspy({ target: '#dados-bar', offset: 600 })
-
-    $('#dados-bar').on('activate.bs.scrollspy', function (event) {
-
-        let ativo = $(event.target).find('a').attr('href')
-
-        switch (ativo) {
-            case `#dados-gerais`:
-                animDadosGerais(ativo)
-                break
-
-            case `#perfil-populacional`:
-                /*
-                  2. Perfil Populacional
-                  - aparecer primeiro o bloco "A população carcerária..."
-                  - aparecer depois o gráfico, animando a linha
-                  - aparecer depois o Estado de São Paulo e preencher proporcionalmente a 35% da área (animação)
-                  - aparecer, por fim, o bloco "segundo dados do IBGE..."
-                */
-                $(`${ativo} .anim1`).addClass(`slideInLeft`)
-                $(`${ativo} svg .linha`).attr(`class`, `linha full`)
-
-                setTimeout(
-                    () => $(`${ativo} .anim2`).addClass(`slideInUp`).removeClass(`anim2`)
-                    , 2500
-                )
-                setTimeout(
-                    () => $(`${ativo} .anim3`).addClass(`slideInUp`).removeClass(`anim3`)
-                    , 3500
-                )
-                break
-
-            case `#infraestrutura`:
-                /*
-               3. Infraestrutura
-               - fixar topo e rodapé (sobre déficit)
-               - aparecer os blocos conforme usuário rolar o scroll
-               */
-                $(`${ativo} .moving`).addClass('anim')
-                break
-
-            case `#situacao-juridica`:
-                contador(document.querySelectorAll('#situacao-juridica [data-final-number]'), 20)
-                /*
-                4. Situação Jurídica
-                - preencher o branco
-                - destacar e aparecer o respectivo texto em branco com animação (contador) do 41%
-                - preencher o vermelho
-                - destacar e aparecer o respectivo texto em vermelho com animação (contador) do 37%
-                - aparecer "1 em cada 5" e depois o restante do rexto "presídio oferece..."
-                - aparecer o último bloco de conteúdo, com apenas os quadrados brancos e com "crimes sem violência" em branco
-                - depois, preencher os semi-círculos vermelhos ao mesmo que tempo que aparece o círculo vermelho
-                  do 7 ao mesmo tempo que  "crimes sem violência" torna-se vermelho
-                */
-                break
-
-            case `#trabalho-educacao`:
-                /* 5. Trabalho e educação
-                - imagem aparece primeiro
-                - depois vem o texto por cima, pelo lado esquerdo
-                - primeiro conjunto de 5 quadradinhos alinhados verticalmente
-                - segundo conjunto de quadradinhos, aparecer num grid de 5 linhas e aparecer coluna por coluna (da esquerda para a direita)
-                - preencher barra de progresso da esquerda para a direita de forma que o 18% seja revelado dentro da barra */
-                contador(document.querySelectorAll('#trabalho-educacao [data-final-number]'), 5)
-                break
-
-            case `#saude`:
-                contador(document.querySelectorAll('#saude [data-final-number]'), 5)
-                /*  6. Saúde
-                - aparecer o texto "consultório médico"
-                - "Saúde" e texto "Apesar da metade..." na mesma cor do atual texto "49 ginecologistas..." com fundo branco
-                - animação (contador) no 49
-                - número 36.495 inciando normal e tornando-se negrito
-                - entrar próxima caixa de texto, sem o "3 vezes..." e os caixões
-                - por fim, aparecer o "3 vezes mais chances de morrer" e os 3 caixoes concomitantemente */
-                break
-
-            case `#materno-infantil`:
-                contador(document.querySelectorAll('#materno-infantil [data-final-number]'), 1)
-                /*  7. Materno-infantil
-                - animação (contador) no 534
-                - animação (contador) no 585 */
-                break
-
-            case `#alas-exclusivas`:
-                contador(document.querySelectorAll('#alas-exclusivas [data-final-number]'), 1)
-                /* 8. Alas Exclusivas
-                - animação (contador) no 12%
-                - número 1.294 inciando normal e tornando-se negrito
-                - preencher celas vermelhas e depois as cadeiras de rodas brancas */
-                break
-
-            case `#unidades-prisionais`:
-
-                break
-
-            default:
-                console.log(`Animation error ${ativo}`)
+    if (location.pathname.toLowerCase() === '/dados/' ) {
+        $('section#title').hide()
+        
+        const animTarget = "#dados-bar"
+        
+        const animDadosGerais = (ativo = `#dados-gerais`) => {
+            /*
+                1. Dados Gerais
+                - número 622 inciando normal e tornando-se negrito
+                - animação (contador) no 111%
+            */
+            $(`${ativo} [data-tobold]`).addClass(`toBold`)
+            contador(
+                document.querySelectorAll(`${ativo} [data-final-number]`)
+                , 12
+                , 1000
+                , () => $(`${ativo} .anim1`).addClass(`bounceInDown`)
+            )
         }
-    })
+
+        const animacoes = function(eventOrHash) {
+            
+            typeof(eventOrHash) === 'string'
+            ? this.ativo = location.hash 
+            : this.ativo = $(eventOrHash.target).find('a').attr('href')
+
+            switch (ativo) {
+                case `#dados-gerais`:
+                    animDadosGerais(this.ativo)
+                    break
+
+                case `#perfil-populacional`:
+
+                    /*
+                        2. Perfil Populacional
+                        - aparecer primeiro o bloco "A população carcerária..."
+                        - aparecer depois o gráfico, animando a linha
+                        - aparecer depois o Estado de São Paulo e preencher proporcionalmente a 35% da área (animação)
+                        - aparecer, por fim, o bloco "segundo dados do IBGE..."
+                    */
+                    $(`${this.ativo} .anim1`).addClass(`slideInLeft`)
+                    $(`${this.ativo} svg .linha`).attr(`class`, `linha full`)
+
+                    setTimeout(
+                        () => $(`${this.ativo} .anim2`).addClass(`slideInUp`).removeClass(`anim2`)
+                        , 2500
+                    )
+                    setTimeout(
+                        () => $(`${this.ativo} .anim3`).addClass(`slideInUp`).removeClass(`anim3`)
+                        , 3500
+                    )
+                    break
+
+                case `#infraestrutura`:
+                    /*
+                    3. Infraestrutura
+                    - fixar topo e rodapé (sobre déficit)
+                    - aparecer os blocos conforme usuário rolar o scroll
+                    */
+                    $(`${this.ativo} .moving`).addClass('anim')
+                    break
+
+                case `#situacao-juridica`:
+                    contador(document.querySelectorAll('#situacao-juridica [data-final-number]'), 20)
+                    /*
+                    4. Situação Jurídica
+                    - preencher o branco
+                    - destacar e aparecer o respectivo texto em branco com animação (contador) do 41%
+                    - preencher o vermelho
+                    - destacar e aparecer o respectivo texto em vermelho com animação (contador) do 37%
+                    - aparecer "1 em cada 5" e depois o restante do rexto "presídio oferece..."
+                    - aparecer o último bloco de conteúdo, com apenas os quadrados brancos e com "crimes sem violência" em branco
+                    - depois, preencher os semi-círculos vermelhos ao mesmo que tempo que aparece o círculo vermelho
+                        do 7 ao mesmo tempo que  "crimes sem violência" torna-se vermelho
+                    */
+                    break
+
+                case `#trabalho-educacao`:
+                    /* 5. Trabalho e educação
+                    - imagem aparece primeiro
+                    - depois vem o texto por cima, pelo lado esquerdo
+                    - primeiro conjunto de 5 quadradinhos alinhados verticalmente
+                    - segundo conjunto de quadradinhos, aparecer num grid de 5 linhas e aparecer coluna por coluna (da esquerda para a direita)
+                    - preencher barra de progresso da esquerda para a direita de forma que o 18% seja revelado dentro da barra */
+                    contador(document.querySelectorAll('#trabalho-educacao [data-final-number]'), 5)
+                    break
+
+                case `#saude`:
+                    contador(document.querySelectorAll('#saude [data-final-number]'), 5)
+                    /*  6. Saúde
+                    - aparecer o texto "consultório médico"
+                    - "Saúde" e texto "Apesar da metade..." na mesma cor do atual texto "49 ginecologistas..." com fundo branco
+                    - animação (contador) no 49
+                    - número 36.495 inciando normal e tornando-se negrito
+                    - entrar próxima caixa de texto, sem o "3 vezes..." e os caixões
+                    - por fim, aparecer o "3 vezes mais chances de morrer" e os 3 caixoes concomitantemente */
+                    break
+
+                case `#materno-infantil`:
+                    contador(document.querySelectorAll('#materno-infantil [data-final-number]'), 1)
+                    /*  7. Materno-infantil
+                    - animação (contador) no 534
+                    - animação (contador) no 585 */
+                    break
+
+                case `#alas-exclusivas`:
+                    contador(document.querySelectorAll('#alas-exclusivas [data-final-number]'), 1)
+                    /* 8. Alas Exclusivas
+                    - animação (contador) no 12%
+                    - número 1.294 inciando normal e tornando-se negrito
+                    - preencher celas vermelhas e depois as cadeiras de rodas brancas */
+                    break
+
+                case `#unidades-prisionais`:
+
+                    break
+
+                default:
+                    console.log(`Animation error: ${this.ativo}`)
+            }
+        }
+
+        location.hash
+        ? animacoes(location.hash)
+        : animDadosGerais()
+        
+        $('body')
+                .attr('data-spy', 'scroll')
+                .attr('data-target', animTarget)
+                .addClass('dados')
+                .scrollspy({ target: animTarget, offset: 600 })
+                .on('activate.bs.scrollspy', event => animacoes(event))
+        
+    }
 
     //funcoes
     function hide_all_modals() {
