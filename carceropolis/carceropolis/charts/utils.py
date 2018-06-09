47d88731411489888093eeddf83e3b40ea979032
AndresMRM <andres@inventati.org>
2018-06-09 11:58:38 -0300

    ordena gráficos de UFs do maior para menor #120

--- carceropolis/carceropolis/charts/utils.py
+++ carceropolis/carceropolis/charts/utils.py
@@ -186,7 +186,8 @@ def read_mini_csv(csv_path):
                 .replace(r'^\s*$', np.nan, regex=True)
                 # Remove columns with all nan cells
                 .dropna(axis=1, how='all'))
-        # data = data.sort_values(by=data.columns[1], ascending=True)
+        if data.columns[0] == 'Unidade da Federação':
+            data = data.sort_values(by=data.columns[1], ascending=True)
         content['xname'] = data.columns[0]
         content['ynames'] = list(data.columns[1:])
         content['dados'] = data
