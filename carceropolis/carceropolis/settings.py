7cf385a0b75f8a790a4ff54941a79ee87b517d42
Diego Rabatone Oliveira <diego@ask-ar.xyz>
2018-06-28 20:26:34 -0300

    Moving [dev] site to carceropolis.org.br

--- carceropolis/carceropolis/settings.py
+++ carceropolis/carceropolis/settings.py
@@ -128,7 +128,8 @@ USE_MODELTRANSLATION = False
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dev.carceropolis.org.br"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dev.carceropolis.org.br",
+                 "carceropolis.org.br"]
 if os.getenv("IS_PRODUCTION"):
     ALLOWED_HOSTS.insert(0, "carceropolis.org.br")
 
