00e31b2de2bbfcc108bf61d0bf9cd837f92f19e7
AndresMRM <andres@inventati.org>
2018-07-15 12:14:24 -0300

    adiciona traducão para home e áreas de atuação #50

--- carceropolis/carceropolis/translation.py
+++ carceropolis/carceropolis/translation.py
@@ -1,9 +1,14 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import Publicacao
+from .models import Publicacao, AreaDeAtuacao
+
+
+class TranslatedAreaDeAtuacao(TranslationOptions):
+    fields = ('nome', 'descricao')
 
 
 class TranslatedPublicacao(TranslationOptions):
     fields = ()
 
 
+translator.register(AreaDeAtuacao, TranslatedAreaDeAtuacao)
 translator.register(Publicacao, TranslatedPublicacao)
